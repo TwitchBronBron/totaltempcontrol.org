fd701a8c53722442ecf4d52375d47d7118e62dac
Bronley Plumb <bronley@gmail.com>
2024-03-19 22:54:26 -0400

    Add gallery comments

--- scripts/generate-gallery.ts
+++ scripts/generate-gallery.ts
@@ -1,3 +1,24 @@
 import * as fastGlob from 'fast-glob';
+import * as fs from 'fs';
+const galleryStart = '<!--gallery-start-->';
+const galleryEnd = '<!--gallery-end-->';
 
-const files = fastGlob.sync('**/*.jpg');
+process.chdir(__dirname + '/..');
+
+let indexHtml = fs.readFileSync('index.html').toString();
+
+const startIndex = indexHtml.indexOf(galleryStart) + galleryStart.length;
+const endIndex = indexHtml.indexOf(galleryEnd) + galleryEnd.length;
+
+const files = fastGlob.sync('images/gallery/**/*.*');
+
+const imageHtml = files.map((file) => {
+    return `
+        <a href="${file}" target="_blank">
+            <img src="${file}" alt="${file}" />
+        </a>
+    `;
+});
+
+indexHtml = indexHtml.substring(0, startIndex) + '\n' + imageHtml.join('') + '\n' + indexHtml.substring(endIndex);
+fs.writeFileSync('index.html', indexHtml);
